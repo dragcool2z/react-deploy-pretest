22462b8c708da2b8e310febc13aabb18e9a72137
Galih A Ramitan <balmough@gmail.com>
2019-09-03 18:16:32 +0700

    Updates

---
+++ precache-manifest.e262dae0a707a7d319ca5bdea5cd5538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "107fba1f18465944c01873d506a715c3",
+    "url": "//dragcool2z.github.io/react-deploy-pretest/index.html"
+  },
+  {
+    "revision": "1b3db195b9dd4b45d4e0",
+    "url": "//dragcool2z.github.io/react-deploy-pretest/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "99420c88f6980ff15b3f",
+    "url": "//dragcool2z.github.io/react-deploy-pretest/static/css/main.5975c50a.chunk.css"
+  },
+  {
+    "revision": "1b3db195b9dd4b45d4e0",
+    "url": "//dragcool2z.github.io/react-deploy-pretest/static/js/2.1b310d3b.chunk.js"
+  },
+  {
+    "revision": "99420c88f6980ff15b3f",
+    "url": "//dragcool2z.github.io/react-deploy-pretest/static/js/main.90ecfae1.chunk.js"
+  },
+  {
+    "revision": "c458da6672c118ba65b2",
+    "url": "//dragcool2z.github.io/react-deploy-pretest/static/js/runtime~main.5725282e.js"
+  }
+]);
